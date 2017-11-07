38c1b47c2c186b2b6833122a80a28c31341f16c3
Pavel Perminov <user100@svd-ad01-ws7001.ad.ies-holding.com>
2017-11-07 17:06:00 +0500

    Some perfomance improvements

--- python_logs_sql.py
+++ python_logs_sql.py
@@ -35,10 +35,6 @@ def mysq_recreate_tables(database):
     database_cursor.close()
     database.commit()
 
-# recreate_tables(database().cursor())
-#
-# exit()
-
 
 def database_cursor(db_file):
     db = sqlite3.connect(db_file)
@@ -63,80 +59,77 @@ def filelist(folder):
     return file_list
 
 
-def prepare_log_from_file(log, database):
-    database_cursor = database.cursor()
-
+def logs_parse(LOG, DB=None):
+    DB_cur = DB.cursor()
     def parse(line):
-        bad_fields = ['#Date:', '#Software:', '#Fields:', '#Version:']
-        for field in bad_fields:
-            if field in line:
-                return
-        item = line.split('\n')[0].split('\t')
-        date = list(map(lambda x: int(x), item[2].split('-')))
-        time = list(map(lambda x: int(x), item[3].split(':')))
-        client_ip = item[0]
-        client_username = item[1]
-        # client_time = dt(date[0], date[1], date[2],
-        #                  time[0], time[1], time[2]).strftime('%s')
-        client_time = (item[2] + " " + item[3])
-        server = item[4]
-        r_ip = item[6]
-        r_port = item[7]
-        recv = item[8]
-        send = item[9]
-        proto = item[10]
-        url = item[11]
-        r_host = res(
-            '^[htp]{0,4}[s]?[\:\/\/]{0,3}([\w\.\-]*)', url).groups()[0]
-        status = item[13]
-        return (server, client_username, client_time,
-                client_ip, r_ip, int(r_port), int(recv), int(send), status, r_host, url)
+        try:
+            item = line.split('\t')
+            client_time = (item[2] + " " + item[3])
+            client_ip = item[0]
+            client_username = item[1]
+            server = item[4]
+            r_ip = item[6]
+            r_port = item[7]
+            recv = item[8]
+            send = item[9]
+            proto = item[10]
+            url = item[11]
+            r_host = res(
+                '^[htp]{0,4}[s]?[\:\/\/]{0,3}([\w\.\-]*)', url).groups()[0]
+            status = item[13]
+            return (server, client_username, client_time,
+                    client_ip, r_ip, int(r_port), int(recv), int(send), status, r_host, url)
+        except:
+            print(line)
 
-    def check_lines(line_to_check):
+    lines_array = []
+    t = time()
+    with open(LOG, 'r') as log_file:
         bad_fields = ['#Date:', '#Software:', '#Fields:', '#Version:']
-        for field in bad_fields:
-            if field in line_to_check:
-                return False
-        return True
-    lines_counter = 0
-    with open(log, 'r') as f:
-        start_time = time()
         while True:
-            lines_counter += 1
-            line = f.readline()
-            if line == '':
-                break
-            if check_lines(line) is True:
-                current_line = parse(line)
-                try:
-                    database_cursor.execute(
-                        'insert into prx values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', current_line)
-                except:
-                    logging.debug(current_line)
-                if lines_counter % 10000 == 0:
-                    database.commit()
-                    end_time = time()
-                    delta_time = end_time - start_time
-                    msg = '100 walked in ' + str(delta_time) + " seconds"
-                    logging.info(msg)
-                    start_time = time()
-
-            else:
+            line = log_file.readline()
+            if ('#Date:' in line) or ('#Software:' in line) or ('#Fields:' in line) or ('#Version:' in line):
                 continue
+            if (line == ''):
+                break
+            lines_array.append(line)
+    print(time() - t, 'reading end')
+    bd = []
+    tmp_1 = 0
+    t-time()
+    for line in lines_array:
+        tmp_1 += 1
+        if tmp_1 == 5000:
+            DB.commit()
 
-    # remove(log)
-    database_cursor.close()
-    database.commit()
-    database.close()
+            msg='5000 walked in '+str(time()-t)
+            print(msg)
+            logging.info(msg)
+            tmp_1=1
+            t=time()
+        current_line = parse(line)
+        DB_cur.execute(
+            'insert into prx values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',
+            current_line)
+    t = time()
+    DB.commit()
+    DB_cur.close()
 
+    DB.close()
+    print(time() - t, 'to commit to SQL')
 
-mysq_recreate_tables(mysql_database())
 
 logging.basicConfig(filename='current_' + str(time()) +
                     ".log", level=logging.DEBUG)
+D1 = mysql_database()
+mysq_recreate_tables(D1)
 
 for filea in filelist(folder):
     # t = time()
     # print(t)
-    prepare_log_from_file(filea[0] + "/" + filea[1], mysql_database())
-    # print(time() - t)
+    logs_parse(filea[0] + "/" + filea[1], mysql_database())
+# print(time() - t)
+# logs_parse('LOGS/ISALOG_20171005_WEB_000.w3c', mysql_database())
+
+# logs_parse('log1',D1)
+# exit()
