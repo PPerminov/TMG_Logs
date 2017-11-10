9d2faa9cc1461d3a39441d20db533a708c3564cf
Pavel Perminov <user100@svd-ad01-ws7001.ad.ies-holding.com>
2017-11-10 15:35:45 +0500

    Worked. But still lot to do Many fixes Changes in structure for logging and perfomance Changes for usability

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 TMG Web logs parser
+Version: 0.0.3 beta
+
+
+
+TODO:
+1. Dynamic logs parsing
+2. Logging fix for more flexibility
+3. Remove TMG logs after commiting
--- python_logs_sql.py
+++ python_logs_sql.py
@@ -1,24 +1,44 @@
 #!/usr/bin/env python3
-from os.path import islink, realpath, abspath
-from os import remove
-# from datetime import datetime as dt
+from os.path import dirname, islink, realpath, abspath
+from os import walk, remove
 from time import time
-from numpy import matrix
 from re import search as res
-import sqlite3
-from pymysql import Connection as connection
 from sys import exit
-import passwd_local
+import variables
+import inspect
 import logging
+import multiprocessing as mp
 
-db_file = 'prx.db'
+
+def log(type, func_name, status, start_time, end_time=None, extra_msg=''):
+    d1 = ' '
+    d2 = ' - '
+    if status == 'started':
+        time_delta = str(start_time)
+        atin = 'at'
+    else:
+        time_delta = str(end_time - start_time)
+        atin = 'in'
+    msg = func_name + d1 + status + d1 + atin + d1 + time_delta + d2 + extra_msg
+    if type == 'debug':
+        logging.debug(msg)
+    elif type == 'info':
+        logging.info(msg)
+    else:
+        return False
+    return start_time
 
 
 def mysql_database():
-    return connection(unix_socket='/var/run/mysqld/mysqld.sock', user=passwd_local.db_user, password=passwd_local.db_pass, db=passwd_local.db_name)
+    func_name = inspect.getframeinfo(inspect.currentframe()).function
+    log('info', func_name, 'started', time())
+    from pymysql import Connection as connection
+    return connection(unix_socket='/var/run/mysqld/mysqld.sock', user=variables.db_user, password=variables.db_pass, db=variables.db_name)
 
 
 def mysq_recreate_tables(database):
+    func_name = inspect.getframeinfo(inspect.currentframe()).function
+    stime = log('info', func_name, 'started', time())
     database_cursor = database.cursor()
     try:
         database_cursor.execute('drop table prx')
@@ -34,56 +54,53 @@ def mysq_recreate_tables(database):
         'CREATE INDEX prx_index ON prx (recv,user,source_ip)')
     database_cursor.close()
     database.commit()
-
-
-def database_cursor(db_file):
-    db = sqlite3.connect(db_file)
-    create_table = "CREATE TABLE IF NOT EXISTS prx(server CHARACTER(15), USER CHARACTER, d_time INTEGER, source_ip CHARACTER(16), dest_ip CHARACTER(16), dest_port INTEGER, recv INTEGER,send INTEGER, action VARCHAR, r_host VARCHAR, url TEXT)"
-    create_index = "CREATE INDEX IF NOT EXISTS prx_index ON prx (recv,user,r_host)"
-    db.execute(create_table)
-    db.execute(create_index)
-    return db
-
-
-folder = abspath('./LOGS')
+    log('info', func_name, 'ended', stime, time())
 
 
 def filelist(folder):
-    from os import walk, path
+    func_name = inspect.getframeinfo(inspect.currentframe()).function
+    stime = log('info', func_name, 'started', time())
     file_list = list()
-    for root, subdirs, file_name in walk(path.abspath(folder)):
+    for root, subdirs, file_name in walk(abspath(folder)):
         if file_name:
             for fn in file_name:
-                current_object = [root, fn]
-                file_list.append(current_object)
+                file_list.append(root + "/" + fn)
+    log('info', func_name, 'ended', stime, time())
     return file_list
 
 
-def logs_parse(LOG, DB=None):
-    DB_cur = DB.cursor()
-    def parse(line):
-        try:
-            item = line.split('\t')
-            client_time = (item[2] + " " + item[3])
-            client_ip = item[0]
-            client_username = item[1]
-            server = item[4]
-            r_ip = item[6]
-            r_port = item[7]
-            recv = item[8]
-            send = item[9]
-            proto = item[10]
-            url = item[11]
-            r_host = res(
-                '^[htp]{0,4}[s]?[\:\/\/]{0,3}([\w\.\-]*)', url).groups()[0]
-            status = item[13]
-            return (server, client_username, client_time,
-                    client_ip, r_ip, int(r_port), int(recv), int(send), status, r_host, url)
-        except:
-            print(line)
+def parse6(line):
+ #   func_name = inspect.getframeinfo(inspect.currentframe()).function
+    #    stime = log('info', func_name, 'started', time())
+    try:
+        item = line.split('\t')
+        client_time = (item[2] + " " + item[3])
+        client_ip = item[0]
+        client_username = item[1]
+        server = item[4]
+        r_ip = item[6]
+        r_port = item[7]
+        recv = item[8]
+        send = item[9]
+        proto = item[10]
+        url = item[11]
+        r_host = res(
+            '^[htp]{0,4}[s]?[\:\/\/]{0,3}([\w\.\-]*)', url).groups()[0]
+        status = item[13]
+        return (server, client_username, client_time,
+                client_ip, r_ip, int(r_port), int(recv), int(send), status, r_host, url)
+    except:
+        logging.debug('Parse error: ' + line)
 
-    lines_array = []
-    t = time()
+
+def logs_parse(LOG):
+    func_name = inspect.getframeinfo(inspect.currentframe()).function
+    stime = log('info', func_name, 'started', time(), extra_msg=LOG)
+    DB_cur = DB.cursor()
+    starttime = time()
+    lines_array = list()
+#    msg = 'File read and parse started - ' + LOG + ' - ' + str(starttime)
+#    logging.info(msg)
     with open(LOG, 'r') as log_file:
         bad_fields = ['#Date:', '#Software:', '#Fields:', '#Version:']
         while True:
@@ -93,45 +110,51 @@ def logs_parse(LOG, DB=None):
             if (line == ''):
                 break
             lines_array.append(line)
-    print(time() - t, 'reading end')
-    bd = []
-    tmp_1 = 0
-    t-time()
-    for line in lines_array:
-        tmp_1 += 1
-        if tmp_1 == 5000:
-            DB.commit()
+    read_end = time()
+    array1 = []
+    for item in lines_array:
+        array1.append(parse6(item))
+ #   t = time()
+    for item in array1:
+        try:
+            DB_cur.execute(
+                'insert into prx values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', item)
+        except:
+            logging.debug(''.join(item))
 
-            msg='5000 walked in '+str(time()-t)
-            print(msg)
-            logging.info(msg)
-            tmp_1=1
-            t=time()
-        current_line = parse(line)
-        DB_cur.execute(
-            'insert into prx values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',
-            current_line)
-    t = time()
-    DB.commit()
+    end_time = time()
+  #  print('commited in', end_time - t)
+    log('info', func_name, 'ended', time(), end_time)
+  #  logging.info('completed file ' + LOG + ' in ' +
+    # str(end_time - starttime) + ' seconds')
     DB_cur.close()
 
 
-    print(time() - t, 'to commit to SQL')
+def killall():
+    global f_pool
+    global DB
+    f_pool.terminate()
+    DB.close()
+    exit()
 
 
-logging.basicConfig(filename='current_' + str(time()) +
-                    ".log", level=logging.DEBUG)
-D1 = mysql_database()
-mysq_recreate_tables(D1)
-
-for filea in filelist(folder):
-    # t = time()
-    # print(t)
-    logs_parse(filea[0] + "/" + filea[1], mysql_database())
-D1.commit()
-D1.close()
-# print(time() - t)
-# logs_parse('LOGS/ISALOG_20171005_WEB_000.w3c', mysql_database())
-
-# logs_parse('log1',D1)
-# exit()
+if __name__ == '__main__':
+    root_dir = dirname(abspath(__file__))
+    logging.basicConfig(filename=abspath(root_dir + variables.logs_folder) + '/current_' + str(time()) +
+                        ".log", level=logging.DEBUG)
+    try:
+        DB = mysql_database()
+        mysq_recreate_tables(DB)
+        files = filelist(variables.source_folder)
+        # f_pool = mp.Pool(mp.cpu_count()-1)
+        f_pool = mp.Pool(4)
+        f_pool.map(logs_parse, files)
+        f_pool.join()
+        f_pool.close()
+        try:
+            DB.commit()
+        except:
+            log.debug('commit problems')
+        DB.close()
+    except KeyboardInterrupt:
+        killall()
