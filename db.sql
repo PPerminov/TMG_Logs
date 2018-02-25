e102a4f828b740cd4312d07f35c02464402cf4e7
Pavel Perminov <pavel@perminov.im>
2018-02-25 16:26:11 +0500

    1. Removed addidtional config module 2. Added parser class 3. Completely changed sql architecture

--- db.sql
+++ db.sql
@@ -1,75 +1,66 @@
--- drop index c__ip_index on proxies.tmp;
-drop table if exists proxies.tmp;
+CREATE TABLE main (
+id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+date__time datetime NOT NULL,
+user SMALLINT UNSIGNED NOT NULL,
+server TINYINT UNSIGNED NOT NULL,
+ip MEDIUMINT UNSIGNED NOT NULL,
+cs BIGINT UNSIGNED NOT NULL,
+sc BIGINT UNSIGNED NOT NULL,
+status TINYINT UNSIGNED,
+connection_type TINYINT UNSIGNED NOT NULL,
+uri VARCHAR(24000),
+r_server TINYINT UNSIGNED NOT NULL,
+rule SMALLINT UNSIGNED,
+agent SMALLINT UNSIGNED);
+
+CREATE INDEX date_time_index ON main (date__time);
 
 
-CREATE TABLE proxies.tmp (
-counter bigint unsigned PRIMARY KEY AUTO_INCREMENT NOT NULL,
-c__ip varchar(16),
-cs__username varchar(255),
-c__agent text,
-sc__authenticated varchar(1),
-date date,
-TIME TIME,
-s__svcname varchar(100),
-s__computername varchar(26),
-cs__referred text,
-r__host varchar(150),
-r__ip varchar(16),
-r__port smallint unsigned,
-time__taken int,
-sc__bytes int,
-cs__bytes int,
-cs__protocol varchar(32),
-cs__transport varchar(6),
-s__operation varchar(32),
-cs__uri text,
-cs__mime__type varchar(50),
-s__object__source varchar(255),
-sc__status smallint unsigned,
-s__cache__info varchar(10),
-RULE text,
-filterinfo text,
-cs__network varchar(255),
-sc__network varchar(255),
-error__info varchar(10),
-action varchar(64),
-gmt__time datetime,
-authenticationserver varchar(64),
-nis__scan__result varchar(64),
-nis__signature varchar(64),
-threatname varchar(64),
-malwareinspectionaction varchar(64),
-malwareinspectionresult varchar(64),
-urlcategory varchar(64),
-malwareinspectioncontentdeliverymethod varchar(64),
-mi__uagarrayid varchar(64),
-sc__uagversion varchar(64),
-mi__uagmoduleid varchar(64),
-sc__uagid varchar(64),
-mi__uagseverity varchar(64),
-mi__uagtype varchar(64),
-sc__uageventname varchar(64),
-mi__uagsessionid varchar(64),
-mi__uagtrunkname varchar(64),
-mi__uagservicename varchar(64),
-sc__uagerrorcode varchar(64),
-malwareinspectionduration varchar(64),
-malwareinspectionthreatlevel varchar(64),
-internal__service__info varchar(64),
-nis__application__protocol varchar(64),
-nat__address varchar(64),
-urlcategorizationreason varchar(64),
-sessiontype varchar(64),
-urldesthost varchar(255),
-s__port smallint unsigned,
-softblockaction varchar(255));
+CREATE TABLE connection_type (
+  id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  value VARCHAR(64) UNIQUE NOT NULL
+);
 
 
-CREATE INDEX c__ip_index ON proxies.tmp (c__ip);
-CREATE INDEX username_index ON proxies.tmp (cs__username);
-CREATE INDEX date_index on proxies.tmp (date);
-CREATE INDEX time_index on proxies.tmp (time);
-create index sc_bytes_index on proxies.tmp (sc_bytes);
+CREATE TABLE server (
+  id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  value VARCHAR(64) UNIQUE NOT NULL
+);
 
 
--- CREATE INDEX c__ip_index ON tmp (c__ip);
+CREATE TABLE status (
+  id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  value VARCHAR(16) UNIQUE NOT NULL
+);
+
+
+CREATE TABLE rule (
+  id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  value VARCHAR(128) UNIQUE NOT NULL
+);
+
+
+CREATE TABLE agent (
+  id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  value VARCHAR(256) UNIQUE NOT NULL
+);
+
+
+CREATE TABLE user (
+  id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  value VARCHAR(64) UNIQUE NOT NULL
+);
+CREATE INDEX user_index ON user (value);
+
+CREATE TABLE r_server (
+  id TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  value VARCHAR(64) UNIQUE NOT NULL
+);
+CREATE INDEX r_server_index on r_server (value);
+
+
+CREATE TABLE ip (
+  id MEDIUMINT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  value VARCHAR(16) UNIQUE NOT NULL
+);
+CREATE INDEX ip_index ON ip (value);
--- fields.parser.py
+++ fields.parser.py
@@ -1,9 +1,83 @@
+from configparser import ConfigParser as Config
 from time import time
-import multiprocessing as mp
-import re
-import sys
-import os
-import local_config
+from os import path
+from os import walk
+from multiprocessing import Pool as pool
+from re import sub
+from sys import exit
+from getpass import getpass
+
+
+class parser:
+
+    def __init__(self, config_file='config.ini'):
+        self.config_file = config_file
+        self.config()
+        self.sql_id = self.sql()
+        self.cursor_id = self.sql_id.cursor()
+        self.sql_main = self.sql()
+        self.cursor_main = self.sql_main.cursor()
+
+    def config(self):
+        self.config = Config()
+        if not path.isfile(self.config_file):
+            self.create_new_config()
+        else:
+            self.config.read(self.config_file)
+
+    def create_new_config(self):
+        # config = Config()
+        params = {}
+        params[server] = input('Mysql server: ')
+        params[database] = input('Database name: ')
+        params[username] = input('Username: ')
+        params[password] = getpass('Password: ')
+        params[source] = input('Logs folder: ')
+        config.add_section('Main')
+        for name, item in params.items():
+            config.set('Main', name, item)
+        with open(config_file, 'w') as config_file:
+            config.write(config_file)
+        self.config = config
+
+    def sql(self):
+        from pymysql import Connection as connection
+        return connection(
+            self.config.get('Main', 'address'),
+            self.config.get('Main', 'username'),
+            self.config.get('Main', 'password'),
+            self.config.get('Main', 'database')
+        )
+
+    def id(self, data, table):
+        cursor = self.cursor_id  # create cursor
+        query = cursor.execute('select id from {table} where {table} = "{data}";'.format(
+            table=table, data=data))  # check for data existence
+        if query == 0:  # if not
+            try:
+                cursor.execute(
+                    'insert into {table} ({table}) values ("{data}");')  # inserting data
+                self.sql_id.commit()
+                # cursor.commit()
+            except:
+                return False  # return False if failed
+            return get_set_id(sql, data, table)  # return recursion for requery
+        return query.fetchone()  # returning ID
+
+    def get_data(self, string, fields, columns):
+        data = {}
+        for item in columns:
+            data[item] = string[fields[item]]
+        return data
+
+    # face_main = get_data(string, fields, columns_main)
+    # face_tabled = get_data(string, fields, columns_tabled)
+
+    def create_query_data(sql, face_main, face_tabled, columns_main, columns_tabled):
+        result_values = []
+        for item in columns_main + columns_tabled:
+            result_values.append()
+        cursor = sql.cursor()
 
 
 def parser(params):
@@ -11,11 +85,6 @@ def parser(params):
     config = params['config']
     proxy_fields = (config.get('Fields', 'proxy_fields')).split(' ')
 
-    def sql(type='local', address='localhost', username='root', password=None, database='proxies'):
-        from pymysql import Connection as connection
-        if type != 'local':
-            return connection(host=address, user=username, password=password, db=database)
-        return connection(unix_socket=address, user=username, password=password, db=database)
     sql = sql(type=1, address=config.get('DataBase', 'address'), username=config.get('DataBase', 'username'),
               password=config.get('DataBase', 'password'), database=config.get('DataBase', 'database'))
     cursor = sql.cursor()
@@ -82,14 +151,14 @@ def parser(params):
 def start():
     files = []
     config = local_config.config()
-    directory = os.path.realpath(config.get('Source', 'source'))
-    all_list = os.walk(directory)
+    directory = path.realpath(config.get('Source', 'source'))
+    all_list = walk(directory)
     for dirpath, dirnames, filenames in all_list:
         for fileitem in filenames:
             file_item = fileitem.lower()
             if 'isalog' in file_item and 'web' in file_item and 'w3c' in file_item:
                 files.append({'file1': fileitem, 'config': config})
-    f_pool = mp.Pool(2)
+    f_pool = pool(2)
     f_pool.map(parser, files)
     f_pool.close()
     f_pool.join()
